eab4213a36d1b40de2c2c20e640e812665cc2717
praise-laurine <praiselaurine10@gmail.com>
2020-11-28 15:14:54 +0300

    add function to make api call

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_source
 
 @app.route('/')
 def index():
@@ -7,8 +8,14 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    general_news = get_source('general')
+    business_news = get_source('business')
+    sports_news = get_source('sports')
+    technology_news = get_source(technology)
     title = 'Home - Welcome to News Bulletin'
-    return render_template('index.html', title = title)
+    
+    return render_template('index.html', title = title,general=general_news,business=business_news,sports=sports_news,technology_news=technology_news)
+    
 
 @app.route('/articles/<id>')
 def sourceAticles(id):
