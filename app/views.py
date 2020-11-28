d621d6b3af6cee00a80afd63903a780bc7150eda
praise-laurine <praiselaurine10@gmail.com>
2020-11-28 12:57:04 +0300

    added index.html file

--- app/views.py
+++ app/views.py
@@ -7,8 +7,8 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-
-    return render_template('index.html')
+    title = 'Home - Welcome to News Bulletin'
+    return render_template('index.html', title = title)
 
 @app.route('/articles/<id>')
 def sourceAticles(id):
@@ -17,6 +17,7 @@ def sourceAticles(id):
     View the article page function that returns the article details page and its data
     '''
 
-    return render_template('article.html')
+    return render_template('sourceArticles.html')
+ 
 
 
