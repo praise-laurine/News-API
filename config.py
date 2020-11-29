9de0c62a1ec2bff519000ba099d1d6ba0511d4bc
praise-laurine <praiselaurine10@gmail.com>
2020-11-29 17:05:06 +0300

    my commit of requests

---
+++ config.py
@@ -0,0 +1,28 @@
+import os
+class Config:
+    '''
+    General configuration parent class
+    '''
+    NEWS_SOURCES_BASE_URL = 'https://newsapi.org/v2/sources?apiKey=121fa427f0854f19b82d81d3c5f8b5ea'
+    ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?q=bitcoin&apiKey=121fa427f0854f19b82d81d3c5f8b5ea'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    @staticmethod
+    def init_app(app):
+            pass
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+
+}
---
+++ start.sh
@@ -0,0 +1,4 @@
+export NEWS_API_KEY='121fa427f0854f19b82d81d3c5f8b5ea'
+export SECRET_KEY='secret'
+
+python3.6 manage.py server
